9f4b176d9ddab450240694d014849f377da1ddff
nathanieluriri <uririnathaniel@gmail.com>
2025-06-12 09:06:35 +0100

    editing nav bar nicely

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,18 +3,26 @@
 
 import { useState } from "react";
 import Link from "next/link";
-
+import Image from "next/image";
 export function Navbar() {
   const [open, setOpen] = useState(false);
 
   return (
-    <nav className="p-4 border-b">
+    <nav className="p-4 border-b bg-[#FBEEEF]">
       <div className="flex items-center justify-between">
-        <Link href="/" className="text-xl font-bold">
-          MyApp
+            
+        <Link href="/" className="text-xl font-bold underline flex items-center text-[#BE1627]">
+        <Image
+        src={'/globe.png'}
+        alt="Digital Guardians Logo"
+        height={381}
+        width={41}
+        />
+          <p className="w-fit">Digital Guardian</p>
+           
         </Link>
         <button className="md:hidden" onClick={() => setOpen(!open)}>
-          ☰
+          <img src="/menubtn.svg" alt="Menu" className="h-6 w-6" />
         </button>
         <div className="hidden md:flex gap-4">
           <Link href="/about">About</Link>
